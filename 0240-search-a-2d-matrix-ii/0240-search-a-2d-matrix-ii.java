ac730c80e25f6b00a8ecbf157dc24a3794f852e6
aradhanapatidar <gameearadhana9755@gmail.com>
2026-04-02 22:20:05 +0530

    Time: 3 ms (89.02%), Space: 48.5 MB (17.3%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+     int i=0;
+     int j=matrix[0].length-1;
+     while(i<matrix.length&&j>=0){
+        if(target==matrix[i][j])  return true;
+        if(target<matrix[i][j]){
+            j--;
+        }else i++;
+     } 
+     return false;  
+    }
+}
